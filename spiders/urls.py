44da64b1c03b8fa557b883ec1298ad0d80c69816
Morveusz93 <morveusz93@gmail.com>
2022-01-28 10:22:20 +0100

    add routes: home and detail. add basic templates

--- spiders/urls.py
+++ spiders/urls.py
@@ -1,7 +1,7 @@
-from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('<str:id>', views.details, name="spider-details"),
 ]
--- spiders/views.py
+++ spiders/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Spider
 
 
 def home(request):
-    return HttpResponse("siema")
+    spiders = Spider.objects.all()
+    context = {'spiders': spiders}
+    return render(request, 'spiders/index.html', context)
+
+def details(request, id):
+    spider = get_object_or_404(Spider, pk=id)
+    print(spider)
+    context = {'spider': spider}
+    return render(request, 'spiders/details.html', context)
+
+def new(request):
+    return render(request, 'spider-form.html')

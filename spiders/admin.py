922615c2afb251b9b0e8d4a88c2803a992281e1d
Morveusz93 <morveusz93@gmail.com>
2022-01-28 09:23:00 +0100

    add Spider model to db

--- spiders/admin.py
+++ spiders/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Spider
 
-# Register your models here.
+admin.site.register(Spider)
--- spiders/models.py
+++ spiders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Spider(models.Model):
+    genus = models.CharField(max_length=200)
+    species = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    molt = models.IntegerField(blank=True, null=True)
+    size = models.FloatField(blank=True, null=True)
+    added = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}, {self.genus[0]}. {self.species} "

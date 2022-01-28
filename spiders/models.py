0d526f78dc2d125720763f8b2ea09ea819654e10
Morveusz93 <morveusz93@gmail.com>
2022-01-28 10:46:25 +0100

    add bootstrap

--- spiders/models.py
+++ spiders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 
 class Spider(models.Model):
@@ -8,6 +9,7 @@ class Spider(models.Model):
     molt = models.IntegerField(blank=True, null=True)
     size = models.FloatField(blank=True, null=True)
     added = models.DateField(auto_now=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
         return f"{self.name}, {self.genus[0]}. {self.species} "

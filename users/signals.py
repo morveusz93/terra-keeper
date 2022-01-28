fede0db60005bc8a23975f5f4d2c88510fb94b0b
Morveusz93 <morveusz93@gmail.com>
2022-01-28 18:06:44 +0100

    add signals for profile

---
+++ users/signals.py
@@ -0,0 +1,24 @@
+from django.db.models.signals import post_save, post_delete
+from django.contrib.auth.models import User
+from .models import Profile
+
+
+def createProfile(sender, instance, created, **kwargs):
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user=user,
+            name=user.username,
+            email=user.email,
+        )
+        print("profile created")
+
+
+def deleteUser(sender, instance, **kwargs):
+    user = instance.user
+    user.delete()
+    print('deleted user')
+
+
+post_save.connect(createProfile, sender=User)
+post_delete.connect(deleteUser, sender=Profile)

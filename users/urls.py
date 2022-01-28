3a0bea78888fde64262cd77482203664a68cafb4
Morveusz93 <morveusz93@gmail.com>
2022-01-28 17:48:33 +0100

    add profile page

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.users, name="users"),
+    path('', views.index, name="users"),
+    path("<str:id>", views.profile, name="user-details"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from .models import Profile
 
 
-def users(request):
-    return render(request, 'users/users.html')
+def index(request):
+    profiles = Profile.objects.all()
+    context = {"profiles": profiles}
+    return render(request, 'users/profiles.html', context)
+
+
+def profile(request, id):
+    profile = Profile.objects.get(id=id)
+    context = {"profile": profile}
+    return render(request, 'users/details.html', context)
+
 
 def newUser(request):
     return render(request, 'users/user-form')

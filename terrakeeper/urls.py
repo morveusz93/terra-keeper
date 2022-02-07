692eb8cf1a5377a9bcb5ce31495f086afaf6799a
Morveusz93 <morveusz93@gmail.com>
2022-02-07 10:57:14 +0100

    add reset password

--- terrakeeper/urls.py
+++ terrakeeper/urls.py
@@ -3,14 +3,24 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 from terrakeeper.settings import MEDIA_ROOT
 
 
 urlpatterns = [
     path('', include('spiders.urls')),
-    path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    path('admin/', admin.site.urls),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(
+        template_name='reset_password.html'), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(
+        template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=MEDIA_ROOT)
